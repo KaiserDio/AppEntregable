25de3068ee74a2ea118edc23967b98f76ce61c0e
KaiserDio <daniel.kaiser20@gmail.com>
2022-03-29 19:38:49 -0300

    Muestra familiares en la pagina web

--- MVTNelsonRodriguez/AppEntregable/views.py
+++ MVTNelsonRodriguez/AppEntregable/views.py
@@ -11,21 +11,23 @@ from .models import Persona
 
 def familia(self):
 
-    persona1 = Persona(nombre="Juan",apellido="Berna",dni=92568652, fechaNacimiento = datetime.date(1948,11,9))
+    persona1 = Persona(nombre="Juan",apellido="Saraza",dni=92568652, fechaNacimiento = datetime.date(1948,11,9))
     persona1.save()
-    persona2 = Persona(nombre="Maria",apellido="Obertti",dni=20557542, fechaNacimiento =datetime.date(1952,1,19))
+    persona2 = Persona(nombre="Maria",apellido="Malanga",dni=20557542, fechaNacimiento =datetime.date(1952,1,19))
     persona2.save()
-    persona3 = Persona(nombre="German",apellido="Berna",dni=45782356, fechaNacimiento =datetime.date(2000,6,12))
+    persona3 = Persona(nombre="Roberto",apellido="Carlos",dni=45782356, fechaNacimiento =datetime.date(2000,6,12))
     persona3.save()
 
     lista = [persona1,persona2,persona3]
-    listaDicci = []
+    listaPrimera = [persona1.nombre, persona1.apellido, persona1.dni, persona1.fechaNacimiento]
+    listaSegunda = [persona2.nombre, persona2.apellido, persona2.dni, persona2.fechaNacimiento]
+    listaTercera = [persona3.nombre, persona3.apellido, persona3.dni, persona3.fechaNacimiento]
 
-    for persona in lista:
+    """ for persona in lista:
         diccionario = {"nombre":persona.nombre,"apellido":persona.apellido,"dni":persona.dni,"fechanacimiento":persona.fechaNacimiento}
-        listaDicci.append(diccionario)
+        listaDicci.append(diccionario) """
 
-    dicc = {"ListaFamiliar":listaDicci}
+    dicc = {"uno":listaPrimera,"dos":listaSegunda,"tres":listaTercera}
 
     plantilla = loader.get_template("plantilla.html")
     
